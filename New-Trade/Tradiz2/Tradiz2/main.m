9798a943d28b62962f4fb7ac34b474742797a4a6
praveen <praveenhiremath923@gmail.com>
2017-05-08 22:16:49 +0530

    init

---
+++ New-Trade/Tradiz2/Tradiz2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tradiz2
+//
+//  Created by praveen on 5/8/17.
+//  Copyright © 2017 com.xinmobile.Tradiz2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TRAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRAppDelegate class]));
+    }
+}
