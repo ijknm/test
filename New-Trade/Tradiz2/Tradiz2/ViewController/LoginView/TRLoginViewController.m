4bf9ddfd137f43312718be863f25da30d9a43ce7
praveen <praveenhiremath923@gmail.com>
2017-05-08 22:38:59 +0530

    Added initial View Controller

---
+++ New-Trade/Tradiz2/Tradiz2/ViewController/LoginView/TRLoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  TRLoginViewController.m
+//  Tradiz2
+//
+//  Created by praveen on 5/8/17.
+//  Copyright © 2017 com.xinmobile.Tradiz2. All rights reserved.
+//
+
+#import "TRLoginViewController.h"
+
+@implementation TRLoginViewController
+
+@end
