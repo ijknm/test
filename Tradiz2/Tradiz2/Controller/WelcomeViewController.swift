f778bedc95ae78ed262cb835dab43e10e1c62c45
Beautistar <beautistar1112@gmail.com>
2017-05-08 08:34:49 -0700

    Start project

---
+++ Tradiz2/Tradiz2/Controller/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeViewController.swift
+//  Tradiz2
+//
+//  Created by JIS on 5/8/17.
+//  Copyright © 2017 Beautistar. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var fieldView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        initView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func initView() {
+        
+        fieldView.layer.borderColor = UIColor.white.cgColor
+        
+        
+    }
+    
+
+
+}
